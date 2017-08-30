65a0ec66d643151e23a5fec9ff7b8adcb8288c87
Callum Forrester <caf32@kent.ac.uk>
2017-08-30 11:43:39 +0100

    Created some basic tests, just playing at this point

---
+++ mapping/p99/ioc-tests/test_detector_01.py
@@ -0,0 +1,18 @@
+from pkg_resources import require
+require('cothread==2.13')
+import unittest
+from cothread import catools
+
+
+DETECTOR_MODEL = "DC-152Q-C00-FI"
+
+def caget(pv_name):
+    return catools.caget(pv_name)
+
+class TestIoc(unittest.TestCase):
+
+    def test_ioc_active(self):
+        print caget("BL99P-EA-DET-01PortName_RBV")
+
+    def test_ioc_camera_connected(self):
+        self.assertEqual(caget("BL99P-EA-DET-01Model_RBV"), DETECTOR_MODEL, "The IOC has not detected the camera")
