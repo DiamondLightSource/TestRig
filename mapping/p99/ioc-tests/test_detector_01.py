b3fb2c4bd340091b82a8eb74a1a41ec2c11928e6
Callum Forrester <caf32@kent.ac.uk>
2017-08-31 09:19:44 +0100

    Started on CA functions and a common test case, will eventually reset detector to defaults before every test

--- mapping/p99/ioc-tests/test_detector_01.py
+++ mapping/p99/ioc-tests/test_detector_01.py
@@ -6,13 +6,25 @@ import unittest
 
 DETECTOR_MODEL = "DC-152Q-C00-FI"
 
-def caget(pv_name):
-    return catools.caget(pv_name)
+class TestDetector(unittest.TestCase):
+    def setUp(self):
+        pass
 
 class TestIoc(unittest.TestCase):
-
     def test_ioc_active(self):
         print caget("BL99P-EA-DET-01PortName_RBV")
-
     def test_ioc_camera_connected(self):
         self.assertEqual(caget("BL99P-EA-DET-01Model_RBV"), DETECTOR_MODEL, "The IOC has not detected the camera")
+
+class TestCapture(unittest.TestCase):
+    #def test_
+    pass
+
+def caget(pv_names):
+    return catools.caget(pv_names)
+
+def caput_async(pv_names, values):
+    catools.caput(pv_names, values)
+
+def caput_sync(pv_names, values):
+    catools.caput(pv_names, values, wait=True)
