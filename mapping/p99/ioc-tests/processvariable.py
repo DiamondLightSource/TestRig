8b7ebfe03c9fd4169acbf9e50499026aeb924413
Callum Forrester <caf32@kent.ac.uk>
2017-08-31 12:03:26 +0100

    Added function to generate value and default lists

--- mapping/p99/ioc-tests/processvariable.py
+++ mapping/p99/ioc-tests/processvariable.py
@@ -1,3 +1,10 @@
+def generate_defaults_lists(pv_list):
+    return (
+        [pv.get_name() for pv in pv_list],
+        [pv.get_default_value() for pv in pv_list]
+    )
+
+
 class ProcessVariable:
     def __init__(self, name, default_value=0):
         self._name = name
--- mapping/p99/ioc-tests/test_processvariable.py
+++ mapping/p99/ioc-tests/test_processvariable.py
@@ -1,16 +1,55 @@
 import unittest
 from processvariable import *
 
-DEFAULT_PV_NAME = "testname"
-DEFAULT_PV_DEFAULT_VALUE = 123
+SECOND_PV_DEFAULT_VALUE = False
+
+SECOND_PV_NAME = "secondtest"
+
+FIRST_PV_NAME = "testname"
+FIRST_PV_DEFAULT_VALUE = 123
 
 
-
-class TestPvMembers(unittest.TestCase):
+class TestProcessVariable(unittest.TestCase):
     def test_constructor_initializes_name(self):
-        pv = ProcessVariable(DEFAULT_PV_NAME)
-        self.assertEqual(DEFAULT_PV_NAME, pv.get_name())
+        pv = make_default_pv()
+        self.assertEqual(FIRST_PV_NAME, pv.get_name())
 
     def test_constructor_initializes_default_value(self):
-        pv = ProcessVariable(DEFAULT_PV_NAME, DEFAULT_PV_DEFAULT_VALUE)
-        self.assertEqual(DEFAULT_PV_DEFAULT_VALUE, pv.get_default_value())
+        pv = make_default_pv()
+        self.assertEqual(FIRST_PV_DEFAULT_VALUE, pv.get_default_value())
+
+
+class TestPvHelperFunctions(unittest.TestCase):
+    def test_generate_defaults_lists_empty(self):
+        pv_list = []
+        self.assert_names_and_defaults(pv_list, [], [])
+
+    def test_generate_defaults_list_singular(self):
+        pv_list = [make_default_pv()]
+        self.assert_names_and_defaults(pv_list, [FIRST_PV_NAME], [FIRST_PV_DEFAULT_VALUE])
+
+    def test_generate_defaults_list_multiple(self):
+        pv_list = make_default_pv_list()
+        expected_names = [FIRST_PV_NAME, SECOND_PV_NAME]
+        expected_default_values = [FIRST_PV_DEFAULT_VALUE, SECOND_PV_DEFAULT_VALUE]
+        self.assert_names_and_defaults(pv_list, expected_names, expected_default_values)
+
+    def assert_names_and_defaults(self, pv_list, pv_names, default_values):
+        names, defaults = generate_defaults_lists(pv_list)
+        self.assertEqual(pv_names, names)
+        self.assertEqual(default_values, defaults)
+
+
+def make_default_pv_list():
+    return [
+        make_default_pv(),
+        make_second_pv()
+    ]
+
+
+def make_default_pv():
+    return ProcessVariable(FIRST_PV_NAME, FIRST_PV_DEFAULT_VALUE)
+
+
+def make_second_pv():
+    return ProcessVariable(SECOND_PV_NAME, SECOND_PV_DEFAULT_VALUE)
