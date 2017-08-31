d746f61a77a14e0497f35b86e9b1543b9a9f7518
Callum Forrester <caf32@kent.ac.uk>
2017-08-31 16:47:32 +0100

    Started on mass-reset to defaults, may not need it after all, will start using Malcolm

--- mapping/p99/ioc-tests/test_detector01.py
+++ mapping/p99/ioc-tests/test_detector01.py
@@ -5,10 +5,15 @@ from processvariable import *
 DETECTOR_MODEL = "DC-152Q-C00-FI"
 PV_MODEL_NAME = ProcessVariable("BL99P-EA-DET-01Model_RBV")
 PV_PORT_NAME = ProcessVariable("BL99P-EA-DET-01PortName_RBV")
-
+PV_DATA_TYPE = ProcessVariable("BL99P-EA-DET-01DataType", "UInt16")
+PV_COLOR_MODE = ProcessVariable("BL99P-EA-DET-01ColorMode", "Mono")
+PV_GAIN = ProcessVariable("BL99P-EA-DET-01Gain", 1.00)
 
 def reset_cam_to_defaults():
-    pass
+    set_pvs_to_defaults([
+        PV_DATA_TYPE,
+        PV_COLOR_MODE
+    ])
 
 
 class TestDetector(unittest.TestCase):
