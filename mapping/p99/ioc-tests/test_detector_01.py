04ccac3c7ea86b456cba2c0b8025728287b1b61f
Callum Forrester <caf32@kent.ac.uk>
2017-08-31 09:21:44 +0100

    Reformatted test_detector_01.py

--- mapping/p99/ioc-tests/test_detector_01.py
+++ mapping/p99/ioc-tests/test_detector_01.py
@@ -3,17 +3,20 @@ from ca_common import *
 
 DETECTOR_MODEL = "DC-152Q-C00-FI"
 
+
 class TestDetector(unittest.TestCase):
     def setUp(self):
         pass
 
+
 class TestIoc(unittest.TestCase):
     def test_ioc_active(self):
         print caget("BL99P-EA-DET-01PortName_RBV")
+
     def test_ioc_camera_connected(self):
         self.assertEqual(caget("BL99P-EA-DET-01Model_RBV"), DETECTOR_MODEL, "The IOC has not detected the camera")
 
-class TestCapture(unittest.TestCase):
-    #def test_
-    pass
 
+class TestCapture(unittest.TestCase):
+    # def test_
+    pass
