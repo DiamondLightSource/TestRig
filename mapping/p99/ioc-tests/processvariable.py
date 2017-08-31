b9e320979918be38723ede0cf6223f18638b1df4
Callum Forrester <caf32@kent.ac.uk>
2017-08-31 09:55:20 +0100

    Added default values to PV class

--- mapping/p99/ioc-tests/processvariable.py
+++ mapping/p99/ioc-tests/processvariable.py
@@ -1,6 +1,10 @@
 class ProcessVariable:
-    def __init__(self, name):
+    def __init__(self, name, default_value=0):
         self._name = name
+        self._default_value = default_value
 
     def get_name(self):
         return self._name
+
+    def get_default_value(self):
+        return self._default_value
--- mapping/p99/ioc-tests/test_processvariable.py
+++ mapping/p99/ioc-tests/test_processvariable.py
@@ -1,10 +1,16 @@
 import unittest
 from processvariable import *
 
-DEFAULT_TEST_PV_NAME = "testname"
+DEFAULT_PV_NAME = "testname"
+DEFAULT_PV_DEFAULT_VALUE = 123
+
 
 
 class TestPvMembers(unittest.TestCase):
     def test_constructor_initializes_name(self):
-        pv = ProcessVariable(DEFAULT_TEST_PV_NAME)
-        self.assertEqual(DEFAULT_TEST_PV_NAME, pv.get_name())
+        pv = ProcessVariable(DEFAULT_PV_NAME)
+        self.assertEqual(DEFAULT_PV_NAME, pv.get_name())
+
+    def test_constructor_initializes_default_value(self):
+        pv = ProcessVariable(DEFAULT_PV_NAME, DEFAULT_PV_DEFAULT_VALUE)
+        self.assertEqual(DEFAULT_PV_DEFAULT_VALUE, pv.get_default_value())
