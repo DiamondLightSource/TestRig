1e4c4f540500208d6960943f47bb068a85222c00
Callum Forrester <caf32@kent.ac.uk>
2017-09-06 13:44:46 +0100

    COmpleted exposure tests, made an acq test that fails

--- mapping/p99/ioc-tests/malcolmtest.py
+++ mapping/p99/ioc-tests/malcolmtest.py
@@ -6,7 +6,24 @@ from malcolm.modules.ADAndor.blocks import andor_detector_driver_block
 class MalcolmTestCase(unittest.TestCase):
     def setUp(self):
         self._process = Process("malcolm-test")
+        self.init_blocks()
         self._process.start()
 
     def tearDown(self):
         self._process.stop()
+
+    def init_blocks(self):
+        pass
+
+
+class AndorDetectorTestCase(MalcolmTestCase):
+    def init_blocks(self):
+        self._detector = self.register_driver_block()
+
+    def register_driver_block(self):
+        mri = "ANDOR"
+        andor_detector_driver_block(self._process, {
+            "mri": mri,
+            "prefix": "BL99P-EA-DET-01:DET:CAM"
+        })
+        return self._process.block_view(mri)
--- mapping/p99/ioc-tests/test_detector01.py
+++ mapping/p99/ioc-tests/test_detector01.py
@@ -3,12 +3,30 @@ from malcolmtest import *
 from malcolm.modules.ADAndor.blocks import andor_detector_driver_block
 
 
-class TestCamera(MalcolmTestCase):
-    def test_set_exposure(self):
-        andor_detector_driver_block(self._process, {
-            "mri": "ANDOR",
-            "prefix": "BL99P-EA-DET-01:DET:CAM"
-        })
-        detector = self._process.block_view("ANDOR")
-        detector.exposure.put_value(0.5)
-        self.assertAlmostEqual(0.5, detector.exposure.value, 4)
+class TestCamera(AndorDetectorTestCase):
+    def test_set_exposure_to_zero(self):
+        self.do_set_exposure(0, 0)
+
+    def test_set_exposure_to_positive(self):
+        self.do_set_exposure(0.1, 0.1)
+
+    def test_set_exposure_to_negative(self):
+        self.do_set_exposure(0, -0.1)
+
+    #def test_set_acquisition_period_greater_than_exposure(self):
+    #    self.do_set_exposure(0.5, 0.5)
+    #    self._detector.acquirePeriod.put_value(0.6)
+    #    self.assert_acquisition_period(0.6)
+
+    def do_set_exposure(self, expected, value):
+        self._detector.exposure.put_value(value)
+        self.assert_exposure(expected)
+
+    def assert_acquisition_period(self, expected):
+        self.assert_almost_equal(expected, self._detector.acquirePeriod.value)
+
+    def assert_exposure(self, expected):
+        self.assert_almost_equal(expected, self._detector.exposure.value)
+
+    def assert_almost_equal(self, expected, actual):
+        self.assertAlmostEqual(expected, actual, 4)
