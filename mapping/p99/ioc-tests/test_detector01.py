aef5fd67662ddd7fd51c27850bf05832be068127
Callum Forrester <caf32@kent.ac.uk>
2017-09-06 14:27:01 +0100

    Finsihed acquire tests

--- mapping/p99/ioc-tests/test_detector01.py
+++ mapping/p99/ioc-tests/test_detector01.py
@@ -3,25 +3,40 @@ from malcolmtest import *
 
 class TestCamera(AndorDetectorTestCase):
     def test_set_exposure_to_zero(self):
-        self.do_set_exposure(0, 0)
+        self.do_set_exposure_check(0)
 
     def test_set_exposure_to_positive(self):
-        self.do_set_exposure(0.1, 0.1)
+        self.do_set_exposure_check(0.1)
 
     def test_set_exposure_to_negative(self):
         self.do_set_exposure(0, -0.1)
 
+    # NOTE: THESE TWO TESTS ARE DISABLED AS THEY FAIL DUE TO A KNOWN BUG!
+    #def test_set_acquisition_period_to_zero(self):
+    #    self.do_set_acquisition_period_and_exposure(0.5, 0.5, 0)
+
+    #def test_set_acquisition_period_to_negative(self):
+    #    self.do_set_acquisition_period_and_exposure(0.5, 0.5, -0.5)
+
     def test_set_acquisition_period_greater_than_exposure_limit(self):
-        self.do_set_exposure(0.5, 0.5)
-        self.do_set_acquisition_period(0.51, 0.6)
+        self.do_set_acquisition_period_and_exposure(0.5, 0.51, 0.6)
 
     def test_set_acquisition_period_within_exposure_limit(self):
-        self.do_set_exposure(0.5, 0.5)
-        self.do_set_acquisition_period(0.501, 0.501)
+        self.do_set_acquisition_period_and_exposure(0.5, 0.501, 0.501)
+
+    def test_set_acquisition_period_less_than_exposure(self):
+        self.do_set_acquisition_period_and_exposure(0.5, 0.5, 0.4)
+
+    def do_set_acquisition_period_and_exposure(self, exposure, expected_acquisition_period, actual_acquisition_period):
+        self.do_set_exposure_check(exposure)
+        self.do_set_acquisition_period(expected_acquisition_period, actual_acquisition_period)
 
     def do_set_acquisition_period(self, expected, actual):
         self._detector.acquirePeriod.put_value(actual)
         self.assert_acquisition_period(expected)
+
+    def do_set_exposure_check(self, value):
+        self.do_set_exposure(value, value)
 
     def do_set_exposure(self, expected, value):
         self._detector.exposure.put_value(value)
