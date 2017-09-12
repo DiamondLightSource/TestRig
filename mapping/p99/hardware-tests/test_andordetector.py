8a78ad5e602ec707ea2f44ea0bc54dc0e7be9eb4
Callum Forrester <caf32@kent.ac.uk>
2017-09-12 15:43:46 +0100

    Fixed the fix

--- mapping/p99/hardware-tests/test_andordetector.py
+++ mapping/p99/hardware-tests/test_andordetector.py
@@ -6,5 +6,5 @@ class AndorDetectorTestCase(AreaDetectorTestCase):
     __test__ = True
 
     def init_blocks(self):
-        self._block_factory.load_blocks("test-yamls/bl99p-ea-det-01.yaml")
+        self._block_factory.load_blocks("test-yaml-configs/bl99p-ea-det-01.yaml")
         self._detector = self._process.block_view("ANDOR:DRV")
