f031ed6705c4c877a95fea82aa5bcc93fd065823
Callum Forrester <caf32@kent.ac.uk>
2017-09-22 12:23:22 +0100

    Minor bugfixing, script will soon be replaced

--- control-automation/webcam-snapshot.py
+++ control-automation/webcam-snapshot.py
@@ -17,13 +17,20 @@ SNAPSHOT_URL = "http://p99-control.diamond.ac.uk:8094/WEB1.mjpg.jpg"
 PREFIX = "BL99P-DI-WEB-01:CAM:"
 IMAGE_MODE = "%sImageMode" % PREFIX
 NUM_IMAGES = "%sNumImages" % PREFIX
-ARRAY_COUNTER = "%sArrayCounter" % PREFIX
+ARRAY_COUNTER = "BL99P-DI-WEB-01:MJPG:ArrayCounter"
 ARRAY_COUNTER_RBV = "%s_RBV" % ARRAY_COUNTER
 ACQUIRE = "%sAcquire" % PREFIX
 JPG_URL = "BL99P-DI-WEB-01:MJPG:JPG_URL_RBV"
 
 
 def capture(path):
+    prepare_camera()
+    capture_single_frame()
+    verify_frame_capture()
+    download_frame(path)
+
+
+def prepare_camera():
     # Set up camera
     print "Setting up camera"
     caput([
@@ -36,12 +43,16 @@ def capture(path):
         0
     ])
 
+
+def capture_single_frame():
     # Capture frame
     print "Capturing a single frame"
     caput(ACQUIRE, 1)
     sleep(2)
     caput(ACQUIRE, 0)
 
+
+def verify_frame_capture():
     # Check frame captured
     if caget(ARRAY_COUNTER_RBV) != 1:
         print "Failed to capture image"
@@ -49,9 +60,13 @@ def capture(path):
     else:
         print "Capture complete"
 
+
+def download_frame(path):
     # Download captured frame
     print "Downloading frame"
+    sleep(2)
     urllib.urlretrieve(SNAPSHOT_URL, path)
+
 
 def caput(pvs, values):
     catools.caput(pvs, values, wait=True)
@@ -63,6 +78,7 @@ def caget(pvs):
 
 def resolve_pv(name):
     return "%s%s" % (PREFIX, name)
+
 
 if __name__ == '__main__':
     path = str(sys.argv[1])
