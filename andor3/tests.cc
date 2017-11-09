2aa423d697db11ced5e3d7a66b830a763b7e79a0
Callum Forrester <caf32@kent.ac.uk>
2017-11-09 14:59:44 +0000

    Refactored common Andor SDK functionality into a utility class, Connection

--- andor3/tests.cc
+++ andor3/tests.cc
@@ -1,73 +1,40 @@
-#include <limits.h>
-#include <stdlib.h>
-#include <iostream>
-#include "atcore.h"
+#include "connection.hpp"
 #include "gtest/gtest.h"
 
 const int DETECTOR_ADDRESS = 0;
 const char* ANDOR_DETECTOR_MODEL = "DC-152Q-C00-FI";
 
-inline void HandleAndorResultCode(int andor_result_code) {
-    EXPECT_EQ(0, andor_result_code);
-}
 
-void InitializeAndorSdk() {
-    int andor_result_code = AT_InitialiseLibrary();
-    HandleAndorResultCode(andor_result_code);
-}
-
-AT_H& OpenCameraConnection() {
-    AT_H handle  = AT_HANDLE_UNINITIALISED;
-    int andor_result_code = AT_Open(DETECTOR_ADDRESS, &handle);
-    HandleAndorResultCode(andor_result_code);
-    return handle;
-}
-
-AT_H& ConnectToCamera() {
-    InitializeAndorSdk();
-    return OpenCameraConnection();
-}
-
-void CloseCameraConnection(AT_H &camera_handle) {
-    int andor_result_code = AT_Close(camera_handle);
-    HandleAndorResultCode(andor_result_code);
-}
-
-void CleanUpAndorSdk() {
-    int andor_result_code = AT_FinaliseLibrary();
-    HandleAndorResultCode(andor_result_code);
-}
-
-void DisconnectFromCamera(AT_H& camera_handle) {
-    CloseCameraConnection(camera_handle);
-    CleanUpAndorSdk();
-}
-
-TEST(Andor, DevicePresent) {
-    AT_H camera_handle = ConnectToCamera();
-
-    AT_WC CameraModel[128];
-    int andor_result_code = AT_GetString(camera_handle, L"Camera Model", CameraModel, 128);
-    if (andor_result_code == AT_SUCCESS) {
-        char szCamModel[128];
-        wcstombs(szCamModel, CameraModel, 64);
-        EXPECT_STREQ(ANDOR_DETECTOR_MODEL, szCamModel);
+class AndorDriverTest : public testing::Test {
+protected:
+    virtual void SetUp() {
+        connection_ = new Connection(DETECTOR_ADDRESS);
+        connection_->Open();
     }
 
-    DisconnectFromCamera(camera_handle);
+    virtual void TearDown() {
+        connection_->Close();
+        delete(connection_);
+    }
+
+    Connection* connection_;
+};
+
+TEST_F(AndorDriverTest, DevicePresent) {
+    AT_H camera_handle = connection_->ConnectionHandle();
+
+    AT_WC CameraModel[128];
+    AT_GetString(camera_handle, L"Camera Model", CameraModel, 128);
+    char szCamModel[128];
+    wcstombs(szCamModel, CameraModel, 64);
+    EXPECT_STREQ(ANDOR_DETECTOR_MODEL, szCamModel);
 }
 
-TEST(Andor, CanSetExposure) {
-    AT_H camera_handle = ConnectToCamera();
+TEST_F(AndorDriverTest, CanSetExposure) {
+    AT_H camera_handle = connection_->ConnectionHandle();
 
-    int andor_result_code = AT_SetFloat (camera_handle,  L"ExposureTime", 0.01);
-    HandleAndorResultCode(andor_result_code);
-
+    AT_SetFloat (camera_handle,  L"ExposureTime", 0.01);
     double exposure_time;
-    andor_result_code = AT_GetFloat(camera_handle, L"ExposureTime", &exposure_time);
-    HandleAndorResultCode(andor_result_code);
-
+    AT_GetFloat(camera_handle, L"ExposureTime", &exposure_time);
     EXPECT_NEAR(0.01, exposure_time, 0.001);
-
-    DisconnectFromCamera(camera_handle);
 }
