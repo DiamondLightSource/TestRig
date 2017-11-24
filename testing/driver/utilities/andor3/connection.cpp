b184869f9f32ef5ce641cefca5f71b50424dbdc3
Callum Forrester <caf32@kent.ac.uk>
2017-11-24 10:22:59 +0000

    Made a new namespace, andor3, and added Connection to it

--- testing/driver/utilities/andor3/connection.cpp
+++ testing/driver/utilities/andor3/connection.cpp
@@ -1,49 +1,49 @@
 #include "connection.hpp"
 
-Connection::Connection(int address) :
+andor3::Connection::Connection(int address) :
     address_(address),
     connection_handle_(new AT_H(AT_HANDLE_UNINITIALISED))
     {
         InitializeAndorSdk();
     }
 
-Connection::~Connection() {
+andor3::Connection::~Connection() {
     Close();
     FinalizeAndorSdk();
 }
 
-void Connection::Open() {
+void andor3::Connection::Open() {
     Close();
     int andor_result_code = AT_Open(address_, &*connection_handle_);
     HandleAndorResultCode(andor_result_code);
 }
 
-void Connection::InitializeAndorSdk() {
+void andor3::Connection::InitializeAndorSdk() {
     int andor_result_code = AT_InitialiseLibrary();
     HandleAndorResultCode(andor_result_code);
 }
 
-void Connection::Close() {
+void andor3::Connection::Close() {
     if(*connection_handle_ != AT_HANDLE_UNINITIALISED)
         CloseSdkAndReset();
 }
 
-void Connection::CloseSdkAndReset() {
+void andor3::Connection::CloseSdkAndReset() {
     int andor_result_code = AT_Close(*connection_handle_);
     HandleAndorResultCode(andor_result_code);
     *connection_handle_ = AT_HANDLE_UNINITIALISED;
 }
 
-void Connection::FinalizeAndorSdk() {
+void andor3::Connection::FinalizeAndorSdk() {
     int andor_result_code = AT_FinaliseLibrary();
     HandleAndorResultCode(andor_result_code);
 }
 
-inline void Connection::HandleAndorResultCode(int andor_result_code) {
+inline void andor3::Connection::HandleAndorResultCode(int andor_result_code) {
     if(andor_result_code)
         throw andor_result_code;
 }
 
-AT_H& Connection::ConnectionHandle() {
+AT_H& andor3::Connection::ConnectionHandle() {
     return *connection_handle_;
 }
--- testing/driver/utilities/andor3/connection.hpp
+++ testing/driver/utilities/andor3/connection.hpp
@@ -6,22 +6,23 @@
 #ifndef DRIVERTESTS_CONNECTION_HPP
 #define DRIVERTESTS_CONNECTION_HPP
 
-
-class Connection {
-private:
-    int address_;
-    std::auto_ptr<AT_H> connection_handle_;
-    void InitializeAndorSdk();
-    void FinalizeAndorSdk();
-    void CloseSdkAndReset();
-    inline void HandleAndorResultCode(int andor_result_code);
-public:
-    Connection(int address);
-    ~Connection();
-    void Open();
-    void Close();
-    AT_H& ConnectionHandle();
-};
+namespace andor3 {
+    class Connection {
+    private:
+        int address_;
+        std::auto_ptr<AT_H> connection_handle_;
+        void InitializeAndorSdk();
+        void FinalizeAndorSdk();
+        void CloseSdkAndReset();
+        inline void HandleAndorResultCode(int andor_result_code);
+    public:
+        Connection(int address);
+        ~Connection();
+        void Open();
+        void Close();
+        AT_H& ConnectionHandle();
+    };
+}
 
 
 #endif //DRIVERTESTS_CONNECTION_HPP
