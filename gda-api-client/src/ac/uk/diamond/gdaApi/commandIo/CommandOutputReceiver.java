68bf759b5e6b5a9380c57e783d720487c95b7d89
Callum Forrester <caf32@kent.ac.uk>
2017-12-05 10:43:50 +0000

    Added command I/O interfaces

---
+++ gda-api-client/src/ac/uk/diamond/gdaApi/commandIo/CommandOutputReceiver.java
@@ -0,0 +1,12 @@
+package ac.uk.diamond.gdaApi.commandIo;
+
+/**
+ * Describes functionality for reading command output from an external console.
+ */
+public interface CommandOutputReceiver {
+    /**
+     * Reads the next available output from the console.
+     * @return A string output.
+     */
+    String readOutput();
+}
