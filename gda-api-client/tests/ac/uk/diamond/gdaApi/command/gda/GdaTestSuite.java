4823fd02b527f4d02c39855d09f92835010da440
Callum Forrester <caf32@kent.ac.uk>
2017-12-12 09:12:12 +0000

    Created GDA test suite

---
+++ gda-api-client/tests/ac/uk/diamond/gdaApi/command/gda/GdaTestSuite.java
@@ -0,0 +1,9 @@
+package ac.uk.diamond.gdaApi.command.gda;
+
+import ac.uk.diamond.gdaApi.command.io.SocketConnectionTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({GdaCommandModelTest.class})
+public class GdaTestSuite { }
