3b1240b2913d4c9192ca1c386e33618c86449758
Callum Forrester <caf32@kent.ac.uk>
2017-12-13 15:47:30 +0000

    Created list command stub, will use to explosre the best way to define a new command

---
+++ gda-api-client/src/ac/uk/diamond/gdaApi/command/gda/listCommand/ListCommand.java
@@ -0,0 +1,20 @@
+package ac.uk.diamond.gdaApi.command.gda.listCommand;
+
+import ac.uk.diamond.gdaApi.command.Command;
+import ac.uk.diamond.gdaApi.command.gda.GdaCommandModel;
+import ac.uk.diamond.gdaApi.command.gda.OutputView;
+import ac.uk.diamond.gdaApi.command.io.CommandOutput;
+import ac.uk.diamond.gdaApi.command.io.CommandModelRunner;
+import ac.uk.diamond.gdaApi.serialization.Deserializer;
+import ac.uk.diamond.gdaApi.serialization.Serializer;
+
+import java.io.IOException;
+
+public class ListCommand implements Command {
+
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ gda-api-client/tests/ac/uk/diamond/gdaApi/command/gda/ListCommandTest.java
@@ -0,0 +1,7 @@
+package ac.uk.diamond.gdaApi.command.gda;
+
+import static org.junit.Assert.*;
+
+public class ListCommandTest {
+
+}
