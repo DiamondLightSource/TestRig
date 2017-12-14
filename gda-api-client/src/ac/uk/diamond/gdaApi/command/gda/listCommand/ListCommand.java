c6f96fabfa1e4df27e88ab4d59d407fde909992b
Callum Forrester <caf32@kent.ac.uk>
2017-12-14 09:18:49 +0000

    Added needed fields to ListCommand

--- gda-api-client/src/ac/uk/diamond/gdaApi/command/gda/listCommand/ListCommand.java
+++ gda-api-client/src/ac/uk/diamond/gdaApi/command/gda/listCommand/ListCommand.java
@@ -2,19 +2,27 @@ package ac.uk.diamond.gdaApi.command.gda.listCommand;
 
 import ac.uk.diamond.gdaApi.command.Command;
 import ac.uk.diamond.gdaApi.command.gda.GdaCommandModel;
-import ac.uk.diamond.gdaApi.command.gda.OutputView;
-import ac.uk.diamond.gdaApi.command.io.CommandOutput;
 import ac.uk.diamond.gdaApi.command.io.CommandModelRunner;
-import ac.uk.diamond.gdaApi.serialization.Deserializer;
-import ac.uk.diamond.gdaApi.serialization.Serializer;
 
 import java.io.IOException;
 
 public class ListCommand implements Command {
+    private CommandModelRunner<GdaCommandModel> runner;
+    private GdaCommandModel commandModel;
 
+    public ListCommand(CommandModelRunner<GdaCommandModel> runner,
+                       GdaCommandModel commandModel) {
+        if  ( runner == null )
+            throw new IllegalArgumentException("Command runner cannot be null");
+        this.runner = runner;
+        if ( commandModel == null )
+            throw new IllegalArgumentException("Command model cannot be null");
+        this.commandModel = commandModel;
+    }
 
     @Override
     public void run() {
 
     }
 }
+
--- gda-api-client/tests/ac/uk/diamond/gdaApi/command/gda/ListCommandTest.java
+++ gda-api-client/tests/ac/uk/diamond/gdaApi/command/gda/ListCommandTest.java
@@ -1,7 +1,43 @@
 package ac.uk.diamond.gdaApi.command.gda;
 
-import static org.junit.Assert.*;
+import ac.uk.diamond.gdaApi.command.gda.listCommand.ListCommand;
+import ac.uk.diamond.gdaApi.command.io.CommandModelRunner;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.mockito.Mockito.mock;
 
 public class ListCommandTest {
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
 
+    private CommandModelRunner<GdaCommandModel> mockRunner;
+    private GdaCommandModel mockModel;
+    private ListCommand command;
+
+    @Before
+    public void setUp() {
+        mockRunner = (CommandModelRunner<GdaCommandModel>)
+                mock(CommandModelRunner.class);
+        mockModel = mock(GdaCommandModel.class);
+        command = new ListCommand(mockRunner, mockModel);
+    }
+
+    @Test
+    public void testRunnerCannotBeNull() {
+        expectIllegalArgumentException();
+        new ListCommand(null, mockModel);
+    }
+
+    @Test
+    public void testModelCannotBeNull() {
+        expectIllegalArgumentException();
+        new ListCommand(mockRunner, null);
+    }
+
+    private void expectIllegalArgumentException() {
+        exception.expect(IllegalArgumentException.class);
+    }
 }
