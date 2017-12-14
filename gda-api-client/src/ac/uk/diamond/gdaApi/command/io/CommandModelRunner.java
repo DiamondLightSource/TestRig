dccfcd8aa861b16f0e182bfbb28d9c941e73a67a
Callum Forrester <caf32@kent.ac.uk>
2017-12-14 09:25:21 +0000

    Amended documentation

--- gda-api-client/src/ac/uk/diamond/gdaApi/command/io/CommandModelRunner.java
+++ gda-api-client/src/ac/uk/diamond/gdaApi/command/io/CommandModelRunner.java
@@ -9,7 +9,6 @@ public interface CommandModelRunner<TCommandModel> {
     /**
      * Runs a command
      * @param command The command to run.
-     * @throws IOException If unable to run.
      */
     void run(TCommandModel command);
 }
