2d9393aa3012f1578c12c0f3988b72047c4463bb
Callum Forrester <caf32@kent.ac.uk>
2018-01-17 12:29:23 +0000

    Developed basic connection method on ssh client facade, does not yet do anything

--- gda-api-client/src/ac/uk/diamond/gdaApi/client/ssh/SshClientFacade.java
+++ gda-api-client/src/ac/uk/diamond/gdaApi/client/ssh/SshClientFacade.java
@@ -1,19 +1,49 @@
 package ac.uk.diamond.gdaApi.client.ssh;
 
+import org.apache.sshd.client.SshClient;
+import org.apache.sshd.client.channel.ClientChannel;
+import org.apache.sshd.client.future.ConnectFuture;
+import org.apache.sshd.client.session.ClientSession;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public class SshClientFacade {
-
+    private final SshClient apacheClient;
     private SshServerDetails serverDetails;
 
-    public SshClientFacade(SshServerDetails serverDetails) {
+    public SshClientFacade(
+            SshServerDetails serverDetails, SshClient apacheClient) {
         if ( serverDetails == null )
             throw new IllegalArgumentException("Server details cannot be null");
+        if ( apacheClient == null )
+            throw new IllegalArgumentException("Apache client cannot be null");
         this.serverDetails = serverDetails;
+        this.apacheClient = apacheClient;
     }
 
     public SshServerDetails getServerDetails() {
         return serverDetails;
+    }
+
+    public void connect(){
+        ConnectFuture future;
+        ClientSession session;
+        ClientChannel channel;
+
+        try {
+            apacheClient.start();
+            future = apacheClient.connect(
+                    serverDetails.getUsername(),
+                    serverDetails.getHostname(),
+                    serverDetails.getPortNumber());
+            future.await();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        } finally {
+            apacheClient.stop();
+        }
+
     }
 }
--- gda-api-client/tests/ac/uk/diamond/gdaApi/client/ssh/SshClientFacadeTest.java
+++ gda-api-client/tests/ac/uk/diamond/gdaApi/client/ssh/SshClientFacadeTest.java
@@ -1,32 +1,118 @@
 package ac.uk.diamond.gdaApi.client.ssh;
 
+import org.apache.sshd.client.SshClient;
+import org.apache.sshd.client.future.ConnectFuture;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.io.IOException;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class SshClientFacadeTest {
-    public static final SshServerDetails SSH_SERVER_DETAILS
-            = mock(SshServerDetails.class);
     @Rule
     public final ExpectedException exception = ExpectedException.none();
+    private SshServerDetails serverDetails;
+    private SshClient apacheClient;
+    private ConnectFuture connectFuture;
+
+    @Before
+    public void setUp() throws IOException {
+        serverDetails = makeMockServerDetails();
+        apacheClient = makeMockApacheClient();
+        connectFuture = mock(ConnectFuture.class);
+        when(apacheClient.connect(anyString(), anyString(), anyInt()))
+                .thenReturn(connectFuture);
+    }
 
     @Test
-    public void testConstructorExceptsNullUsername() {
+    public void testConstructorExceptsNullServerDetails() {
         expectIllegalArgumentException();
-        new SshClientFacade(null);
+        makeFacade(null, makeMockApacheClient());
+    }
+
+    @Test
+    public void testConstructorExceptsNullApacheClient() {
+        expectIllegalArgumentException();
+        makeFacade(makeMockServerDetails(), null);
     }
 
     @Test
     public void testConstructorInitializesServerDetails() {
-        SshClientFacade facade = new SshClientFacade(SSH_SERVER_DETAILS);
-        assertEquals(SSH_SERVER_DETAILS, facade.getServerDetails());
+        SshClientFacade facade = makeDefaultFacade();
+        assertEquals(serverDetails, facade.getServerDetails());
     }
-    
+
+    @Test
+    public void testConnectForwardsExceptionIfPresent() throws IOException {
+        expectRuntimeException();
+        SshClientFacade facade = makeDefaultFacade();
+        when(apacheClient.connect(anyString(), anyString(), anyInt()))
+                .thenThrow(new IOException());
+        facade.connect();
+    }
+
+    @Test
+    public void testConnectStartsClient() throws IOException {
+        SshClientFacade facade = makeDefaultFacade();
+        facade.connect();
+        verify(apacheClient, times(1))
+                .start();
+    }
+
+    @Test
+    public void testConnectConnectsClient() throws IOException {
+        SshClientFacade facade = makeDefaultFacade();
+        facade.connect();
+        verify(apacheClient, times(1))
+                .connect(anyString(), anyString(), anyInt());
+    }
+
+    @Test
+    public void testClientIsStoppedAfterUnsuccessfulConnection()
+            throws IOException {
+        expectRuntimeException();
+        SshClientFacade facade = makeDefaultFacade();
+        when(apacheClient.connect(anyString(), anyString(), anyInt()))
+                .thenThrow(new IOException());
+        facade.connect();
+        verify(apacheClient, times(1)).stop();
+    }
+
+    @Test
+    public void testClientIsStoppedAfterSuccessfulConnection()  {
+        SshClientFacade facade = makeDefaultFacade();
+        facade.connect();
+        verify(apacheClient, times(1)).stop();
+    }
+
+
+    private SshClientFacade makeDefaultFacade() {
+
+        return makeFacade(serverDetails, apacheClient);
+    }
+
+    private SshServerDetails makeMockServerDetails() {
+        return mock(SshServerDetails.class);
+    }
+
+    private SshClient makeMockApacheClient() {
+        return mock(SshClient.class);
+    }
+
+    private SshClientFacade makeFacade(
+            SshServerDetails sshServerDetails, SshClient apacheClient) {
+        return new SshClientFacade(sshServerDetails, apacheClient);
+    }
+
     private void expectIllegalArgumentException() {
         exception.expect(IllegalArgumentException.class);
+    }
+
+    private void expectRuntimeException() {
+        exception.expect(RuntimeException.class);
     }
 }
