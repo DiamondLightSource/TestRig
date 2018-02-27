00e0c1f3a6938702205c7a6a13c460afaf6abef7
Callum Forrester <callum.forrester@diamond.ac.uk>
2018-02-27 09:57:48 +0000

    Simplfied test

--- testing/driver/tests/andor3.cc
+++ testing/driver/tests/andor3.cc
@@ -13,12 +13,12 @@ const std::string ANDOR_DETECTOR_MODEL = "DC-152Q-C00-FI";
  * @param andor_result_code Result code to analyse
  */
 inline void HandleAndorResultCode(int andor_result_code) {
-    EXPECT_EQ(0, andor_result_code);
+    ASSERT_EQ(0, andor_result_code);
 }
 
 /**
  * Helper function for converting a string of type AT_WC* to
- * a higher-level C++ std::string
+ * a C++ std::string
  * @param andor_wide_string The AT_WC* to convert
  * @return A converted string of type std::string
  */
@@ -27,27 +27,6 @@ std::string ConvertAndorWideStringToString(AT_WC* andor_wide_string) {
     return std::string(wide_string.begin(), wide_string.end());
 }
 
-void InitializeAndorSdk() {
-    int andor_result_code = AT_InitialiseLibrary();
-    HandleAndorResultCode(andor_result_code);
-}
-
-void CloseCameraConnection(AT_H& camera_handle) {
-    int andor_result_code = AT_Close(camera_handle);
-    HandleAndorResultCode(andor_result_code);
-}
-
-void OpenCameraConnection(AT_H& camera_handle) {
-    camera_handle = AT_HANDLE_UNINITIALISED;
-    int andor_result_code = AT_Open(DETECTOR_ADDRESS, &camera_handle);
-    HandleAndorResultCode(andor_result_code);
-}
-
-
-void ConnectToCamera(AT_H& camera_handle) {
-    InitializeAndorSdk();
-    OpenCameraConnection(camera_handle);
-}
 
 std::string GetCameraModel(AT_H& camera_handle) {
     AT_WC camera_model[128];
@@ -55,16 +34,6 @@ std::string GetCameraModel(AT_H& camera_handle) {
             camera_handle, L"Camera Model", camera_model, 128);
     HandleAndorResultCode(andor_result_code);
     return ConvertAndorWideStringToString(camera_model);
-}
-
-void CleanUpAndorSdk() {
-    int andor_result_code = AT_FinaliseLibrary();
-    HandleAndorResultCode(andor_result_code);
-}
-
-void DisconnectFromCamera(AT_H& camera_handle) {
-    CloseCameraConnection(camera_handle);
-    CleanUpAndorSdk();
 }
 
 /**
@@ -75,19 +44,24 @@ void DisconnectFromCamera(AT_H& camera_handle) {
 class AndorDriverTest : public testing::Test {
 protected:
     virtual void SetUp() {
-        camera_handle = std::make_unique<AT_H>(AT_HANDLE_UNINITIALISED);
-        ConnectToCamera(*camera_handle);
+        camera_handle = AT_HANDLE_UNINITIALISED;
+        int initialise_result = AT_InitialiseLibrary();
+        HandleAndorResultCode(initialise_result);
+        int open_result = AT_Open(DETECTOR_ADDRESS, &camera_handle);
+        HandleAndorResultCode(open_result);
     }
 
     virtual void TearDown() {
-        DisconnectFromCamera(*camera_handle);
-        delete(camera_handle);
+        int close_result = AT_Close(camera_handle);
+        HandleAndorResultCode(close_result);
+        int finalise_result = AT_FinaliseLibrary();
+        HandleAndorResultCode(finalise_result);
     }
 
-    std::unique_ptr<AT_H> camera_handle;
+    AT_H camera_handle;
 };
 
 TEST_F(AndorDriverTest, DevicePresent) {
-    std::string camera_model = GetCameraModel(*camera_handle);
+    std::string camera_model = GetCameraModel(camera_handle);
     ASSERT_EQ(ANDOR_DETECTOR_MODEL, camera_model);
 }
