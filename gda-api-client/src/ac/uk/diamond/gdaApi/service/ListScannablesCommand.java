f6771cf98fe5aefa576da9bbf1ab0604d0294d90
Callum Forrester <callum.forrester@diamond.ac.uk>
2018-05-03 14:22:30 +0100

    Removed unused class

--- gda-api-client/src/ac/uk/diamond/gdaApi/service/ListScannablesCommand.java
+++
@@ -1,9 +0,0 @@
-package ac.uk.diamond.gdaApi.service;
-
-public class ListScannablesCommand {
-    private static final String COMMAND_STRING = "ls_names";
-
-    public String getCommandString() {
-        return COMMAND_STRING;
-    }
-}
--- gda-api-client/tests/ac/uk/diamond/gdaApi/service/ServiceTestSuite.java
+++ gda-api-client/tests/ac/uk/diamond/gdaApi/service/ServiceTestSuite.java
@@ -4,6 +4,5 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ListScannablesCommandTest.class,
-        ScannableNameListerImplTest.class})
+@Suite.SuiteClasses({ScannableNameListerImplTest.class})
 public class ServiceTestSuite { }
